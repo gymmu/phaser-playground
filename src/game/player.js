6b7e6ffb1fc402e1e89b3e421324f4df3469df53
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-12-26 15:22:11 +0000

    Remove unneeded code.

--- src/game/player.js
+++ src/game/player.js
@@ -3,18 +3,9 @@ import Phaser from "phaser"
 export default class Player extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y, number) {
     super(scene, x, y, "player")
-    this.setOrigin(0.5)
     this.scene.add.existing(this)
-    this.scene.physics.add.existing(this)
+    this.scene.physics.add.existing(this, false)
     this.body.collideWorldBounds = true
-    this.setScale(1)
-
-    this.jumping = false
-    this.invincible = false
-    this.health = 10
-    this.body.mass = 10
-    this.body.setDragY = 10
-    this.body.setDragX = 10
 
     this.setControls()
   }
@@ -23,17 +14,17 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     this.cursor = this.scene.input.keyboard.createCursorKeys()
   }
 
-  update(timestep, delta) {
-    if (this.cursor.left.isDown) {
-      this.body.setVelocityX(-100)
-    }
-    if (this.cursor.right.isDown) {
+  update() {
+    const { body } = this
+    const { left, right, space } = this.cursor
+    if (left.isDown) {
+      body.setVelocityX(-100)
+    } else if (right.isDown) {
       this.body.setVelocityX(100)
-    }
-    if (this.cursor.right.isUp && this.cursor.left.isUp) {
+    } else {
       this.body.setVelocityX(0)
     }
-    if (this.cursor.space.isDown && this.body.onFloor()) {
+    if (space.isDown && this.body.onFloor()) {
       this.body.setVelocityY(-250)
     }
   }
