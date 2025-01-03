951377a1433d1e3ff959be25a306d3c43b1bacb5
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-01-03 12:06:31 +0000

    Move `HelperScene` to own file.

--- src/game/scenes/intro.js
+++ src/game/scenes/intro.js
@@ -2,75 +2,7 @@ import Phaser from "phaser"
 import Player from "../player"
 import Mushroom from "../mushroom"
 import Cave from "../cave"
-
-/**
- * Erweiterung einer Phaser.Scene mit praktischen Funktionen um Spielobjekte
- * automatisch zu erstellen.
- */
-class HelperScene extends Phaser.Scene {
-  /**
-   * Erstellt eine Instanz einer Phaser.Szene.
-   *
-   * @param {*} options Optionen die an eine Szene übergeben werden können.
-   */
-  constructor(options) {
-    super(options)
-  }
-
-  /**
-   * Erstelle ein Spielobjekt an der Stelle des SpawnPoints auf der Karte.
-   *
-   * Diese Funktion wird verwendet um einzelne Objekte zu erstellen, wie zum
-   * Beispiel den Spieler, oder einen Endgegner, sowie Spielobjekte die nur
-   * einmal vorkommen.
-   *
-   * @param {*} map Die Karte in der das Spielobjekt definiert wurde.
-   * @param {String} objectLayer Der Name des Layers in dem das Spielobjekt definiert wurde.
-   * @param {String} objectName Der Name des Objekts, so wie es in der Karte
-   * erstellt wurde (z.B. "SpawnPlayer").
-   * @param {*} objectClass Die Klasse mit dem das neue Objekt erstellt werden
-   * soll (z.B. Player).
-   */
-  createSingleObject(map, objectLayer, objectName, objectClass) {
-    const spawnPoint = map.findObject(
-      objectLayer,
-      (obj) => obj.name === objectName,
-    )
-    return new objectClass(this, spawnPoint.x, spawnPoint.y)
-  }
-
-  /**
-   * Erstelle alle Objekte aus einem Layer der Karte, mit einem bestimmten Typ.
-   * Diese Funktion wird benötigt um Spielobjekte zu erstellen, mit denen ein
-   * Spieler interagieren kann. Also Gegenstände die man aufsammeln kann, oder
-   * auch Türen durch die man läuft.
-   *
-   * Diese Objekte bekommen noch die `Custom Properties` aus Tiled übergeben. So
-   * können verschiedene Eigenschaften auf den Objekten gestezt werden. So
-   * können zum Beispiel die Pilze verschieden viel Schaden anrichten. Man kann
-   * auf Türen auch einstellen in welche Welt man gehen möchte.
-   *
-   * @param {*} map Die Karte in der das Spielobjekt definiert wurde.
-   * @param {String} objectLayer Der Name des Layers in dem das Spielobjekt definiert wurde.
-   * @param {String} objectName Der Name des Objekts, so wie es in der Karte
-   * erstellt wurde (z.B. "Mushroom").
-   * @param {*} objectClass Die Klasse mit dem das neue Objekt erstellt werden
-   * soll (z.B. Mushroom).
-   * @param {*} targetGroup Die Gruppe zu der ein Objekt hinzugefügt wird. Die
-   * Gruppe wird gebraucht um auf kollision mit einem weiteren Objekt zu prüfen.
-   */
-  createObjects(map, objectLayer, objectName, objectClass, targetGroup) {
-    const objects = map.filterObjects(
-      objectLayer,
-      (obj) => obj.name === objectName,
-    )
-    if (objects != null) {
-      objects.forEach((obj) => {
-        targetGroup.add(new objectClass(this, obj.x, obj.y, obj.properties))
-      })
-    }
-  }
-}
+import HelperScene from "./helper-scene"
 
 export default class Intro extends HelperScene {
   obstacles
