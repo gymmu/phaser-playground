52514e76b8d7e5cc23860b01f9736d1e7920a832
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-01-03 05:38:58 +0000

    Add favicon.

--- src/game/plattform.js
+++ src/game/plattform.js
@@ -4,7 +4,6 @@ export default class Plattform extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y) {
     super(scene, x, y, "pickups", "mushroom")
     this.scene.add.existing(this)
-    //this.scene.textures.setTexture("tileset", "mushroom")
     this.scene.physics.add.existing(this)
     this.body.setAllowGravity(false)
     this.body.setImmovable(true)
